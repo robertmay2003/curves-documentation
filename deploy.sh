e86493021dd669725d9a1e57b4b7839c931622c7
Robert May <robertmay2003@gmail.com>
2020-09-13 18:34:04 -0400

    Publishing to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+rm -rf build
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+cd build && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+git push origin gh-pages
