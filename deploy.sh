0293e569548321a6b6f187c1f53754f80fab64f7
Robert May <robertmay2003@gmail.com>
2020-09-13 18:51:57 -0400

    Trying to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rm -rf build
 hugo -D
-cd build && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages && cd ..
+cd build && git add --all && git commit -m "Publishing to gh-pages" && git push && cd ..
